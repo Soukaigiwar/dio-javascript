f8e5ef2c5c3562145c3180fdd146a6c52f095e17
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-05-27 00:48:56 -0300

    funcoes - iniciando

---
+++ funcoes/assets/js/alunos_aprovados.js
@@ -0,0 +1,28 @@
+/*
+
+1 - Crie uma função que recebe o array alunos e um número que irá representar a média final;
+
+2 - Percorra o array e popule um novo array auxiliar apenas com os alunos cujas notas são maiores ou iguais à média final;
+
+3 - Utilize a técnica "object destructuring" para manipular as propriedades desejadas de cada aluno.
+
+*/
+var alunos  = [];
+var notas   = [];
+
+const qtd = prompt("Quantos alunos:");
+
+function add_aluno(qtd = 0){
+    let i = 0
+    do {
+        alunos.push(prompt(`Digite o nome do aluno ${i + 1}:`));
+        notas.push(prompt(`Digite a nota do aluno ${i + 1}:`));
+    }
+    while(++i < qtd);
+}
+
+add_aluno(qtd);
+
+alunos.forEach(console.log(alunos));
+
+3
