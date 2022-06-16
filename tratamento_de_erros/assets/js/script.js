2c4a905c7e0baea8ac302d04e368ed76bbd7dfcb
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-06-16 18:09:54 -0300

    check error

--- tratamento_de_erros/assets/js/script.js
+++ tratamento_de_erros/assets/js/script.js
@@ -1,13 +1,33 @@
-const my_string = document.getElementById('testInputArray');
-number_of_chars = 6;
+function error_handling() {
+    let my_string = document.getElementById('testInputString');
+    let len_parameter = document.getElementById('testInputLength');
+    let errorLabel = document.getElementById('errorLabel');
 
-function error_handling(number_of_chars){
+    console.log(typeof Object(my_string));
     try {
-        if(my_string.length != number_of_chars){
-            console.log("caraculis");
-        }
+        if (!my_string.value && !len_parameter.value.trim())
+            throw new ReferenceError("Envie os campos preenchidos.");
+        if (typeof my_string !== 'object')
+            throw new TypeError('Array precisa ser do tipo object.');
+        if (isNaN(len_parameter.value) || len_parameter.value.trim() == '')
+            throw new TypeError('"Quantos caracteres" precisa ser do tipo número. ');
+        if (my_string.value.length !== Number(len_parameter.value))
+            throw new RangeError('Tamanho Inválido');
+        return errorLabel.innerText = 'Tudo ok por aqui';
+
     } catch (error) {
-        console.log("caraculis");
+        if (error instanceof ReferenceError) {
+            console.log('ReferenceError: ' + error.message)
+            errorLabel.innerText = 'ReferenceError: ' + error.message;
+        } else if (error instanceof TypeError) {
+            console.log('TypeError: ' + error.message)
+            errorLabel.innerText = 'TypeError: ' + error.message;
+        } else if (error instanceof RangeError) {
+            console.log('RangeError: ' + error.message);
+            errorLabel.innerText = 'RangeError: ' + error.message;
+        } else {
+            console.log('Tipo de erro não esperado. ' + error);
+            errorLabel.innerText = 'Tipo de erro não esperado. ' + error;
+        }
     }
-    // console.log(`${my_string.value}awefiodADdA`);
 }
